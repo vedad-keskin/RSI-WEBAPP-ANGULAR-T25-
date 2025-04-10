47ebb951cd7ab22ecfb6d99754e0ca7235c1cf27
Vedad Keskin <vedad.keskin98@gmail.com>
2025-04-10 19:47:40 +0200

    2B

---
+++ Workshops/ISPITNI JAN-21-2025/RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/endpoints/semester-endpoints/semester-update-or-insert-endpoint.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MyConfig } from '../../my-config';
+import { MyBaseEndpointAsync } from '../../helper/my-base-endpoint-async.interface';
+
+
+export interface SemesterUpdateOrInsertRequest {
+  id?: number; // Nullable for insert
+  studentId: number;
+  academicYearId: number;
+  recordedById: number;
+  yearOfStudy: number;
+  renewal: boolean;
+  date: Date;
+  price: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SemesterUpdateOrInsertEndpoint
+  implements MyBaseEndpointAsync<SemesterUpdateOrInsertRequest, number> {
+  private apiUrl = `${MyConfig.api_address}/semesters`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  handleAsync(request: SemesterUpdateOrInsertRequest) {
+    return this.httpClient.post<number>(this.apiUrl, request);
+  }
+}
--- Workshops/ISPITNI JAN-21-2025/RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters-new/student-semesters-new.component.ts
+++ Workshops/ISPITNI JAN-21-2025/RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters-new/student-semesters-new.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {
+  AcademicYearGetAllEndpoint, AcademicYearGetAllResponse
+} from '../../../../../endpoints/academic-year-endpoints/academic-year-get-all-endpoint.service';
+import {tap} from 'rxjs/operators';
+import {
+  SemesterUpdateOrInsertEndpoint, SemesterUpdateOrInsertRequest
+} from '../../../../../endpoints/semester-endpoints/semester-update-or-insert-endpoint.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-student-semesters-new',
@@ -9,33 +17,76 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   templateUrl: './student-semesters-new.component.html',
   styleUrl: './student-semesters-new.component.css'
 })
-export class StudentSemestersNewComponent {
+export class StudentSemestersNewComponent implements OnInit {
 
   semesterForm: FormGroup;
   studentId: any;
+  academicYears: AcademicYearGetAllResponse[] = [];
+  loggedInUserId:any;
 
   constructor(
     private route: ActivatedRoute,
     private router:Router,
     private fb: FormBuilder,
+    private academicYearGetAllEndpoint:AcademicYearGetAllEndpoint,
+    private semesterUpdateOrInsertEndpoint:SemesterUpdateOrInsertEndpoint,
+    private snackBar: MatSnackBar
   ) {
     this.studentId = Number(this.route.snapshot.paramMap.get('id'));
 
+    const authData = localStorage.getItem('my-auth-token');
+    
+    if (authData) {
+      const parsedAuth = JSON.parse(authData);
+      this.loggedInUserId = parsedAuth.myAuthInfo?.userId ?? 0;
+    }
+
+
     this.semesterForm = this.fb.group({
-      academicYearId: [0, [Validators.required]],
+      academicYearId: [1, [Validators.required]],
       studentId: [this.studentId, [Validators.required]],
-      recordedById: [1, [Validators.required]],
+      recordedById: [this.loggedInUserId, [Validators.required]],
       yearOfStudy: [null, [Validators.required]],
       renewal: [false, [Validators.required]],
-      date: [Date.now(), [Validators.required]],
+      //date: [Date.now(), [Validators.required]],
+      date: [new Date(), [Validators.required]],
       price: [null, [Validators.required, Validators.min(50), Validators.max(2000)]],
     });
 
   }
 
+  ngOnInit(): void {
+        this.fetchAcademicYears();
+    }
+
   saveSemester() {
+
+    if (this.semesterForm.invalid) return;
+
+    const semesterData: SemesterUpdateOrInsertRequest = {
+      ...this.semesterForm.value,
+    };
+
+    this.semesterUpdateOrInsertEndpoint.handleAsync(semesterData).subscribe({
+      next: () => {
+        this.snackBar.open('Semester saved successfully!', 'Close', { duration: 5000 });
+        this.router.navigate([`/admin/semesters/${this.studentId}`]);
+      },
+      error: (error) => {
+        this.snackBar.open('Error saving semester. Please try again.', 'Close', { duration: 5000 });
+        console.error('Error saving semester', error);
+      },
+    });
 
   }
 
 
+  private fetchAcademicYears() {
+    this.academicYearGetAllEndpoint.handleAsync()
+      .pipe(tap(x => console.log("fetched: " + x.length)))
+      .subscribe({
+        next: (data) => (this.academicYears = data),
+        error: (err) => console.error('Error fetching semesters:', err)
+      });
+  }
 }
