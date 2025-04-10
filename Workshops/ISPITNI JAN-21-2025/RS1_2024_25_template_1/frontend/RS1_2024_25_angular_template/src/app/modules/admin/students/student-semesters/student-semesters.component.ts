e365ea46d9e1ee20ceaa5741b96b320f232c29b2
Vedad Keskin <vedad.keskin98@gmail.com>
2025-04-10 02:26:51 +0200

    2A.2

--- Workshops/ISPITNI JAN-21-2025/RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Workshops/ISPITNI JAN-21-2025/RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
 
 @Component({
   selector: 'app-student-semesters',
   standalone: false,
-  
+
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit {
 
+  studentId: any;
+  // student:any;
+  student: StudentGetByIdResponse | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private studentGetByIdEndpointService:StudentGetByIdEndpointService
+  ) {
+    this.studentId = Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+  ngOnInit(): void {
+
+    this.fetchStudent();
+
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdEndpointService.handleAsync(this.studentId).subscribe({
+      next: (student) => {
+
+        this.student = student;
+
+      },
+      error: (error) => console.error('Error loading city data', error),
+    });
+
+
+  }
 }
