9982f4176a0e92b7dff480c105c41d252b30a8c7
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-13 17:25:19 +0200

    2C.1 - Izel Repuh

--- Workshops/1. ISPITNI JAN-21-2025/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
+++ Workshops/1. ISPITNI JAN-21-2025/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters.component.ts
@@ -1,5 +1,10 @@
-import {Component, Inject} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {
+  StudentGetByIdEndpointService, StudentGetByIdResponse
+} from '../../../../endpoints/student-endpoints/student-get-by-id-endpoint.service';
+import {MySnackbarHelperService} from '../../../shared/snackbars/my-snackbar-helper.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-student-semesters',
@@ -8,19 +13,48 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './student-semesters.component.html',
   styleUrl: './student-semesters.component.css'
 })
-export class StudentSemestersComponent {
+export class StudentSemestersComponent implements OnInit{
 
   // Nase globalne varijable
   studentId:any;
+  // student:any; // LAKSI NACIN
+  student:StudentGetByIdResponse | null = null;
+
 
 
   constructor(
     // copied from student-edit.component.ts
     private route: ActivatedRoute,
+    private studentGetByIdEndpointService:StudentGetByIdEndpointService,
+    private snackbar: MySnackbarHelperService,
+    private router: Router,
+    private dialog: MatDialog,
   ) {
 
     this.studentId = this.route.snapshot.params['id'];
-
   }
 
+  ngOnInit(): void {
+
+    this.fetchStudent();
+
+    }
+
+
+  private fetchStudent() {
+
+    this.studentGetByIdEndpointService.handleAsync(this.studentId).subscribe({
+      next: (data) => {
+
+        this.student = data;
+
+      },
+      error: (err) => {
+        this.snackbar.showMessage('Error fetching student. Please try again.', 5000);
+        console.error('Error fetching student:', err);
+      }
+    });
+
+
+  }
 }
