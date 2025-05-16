6e5ecc4547c33da402ca12253598774be025c388
Vedad Keskin <vedad.keskin98@gmail.com>
2025-05-16 04:15:42 +0200

    2B - Basic adding semester

--- Workshops/TEST/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters-new/student-semesters-new.component.ts
+++ Workshops/TEST/frontend/RS1_2024_25_angular_template/src/app/modules/admin/students/student-semesters/student-semesters-new/student-semesters-new.component.ts
@@ -8,6 +8,14 @@ import {MatDialog} from '@angular/material/dialog';
 import {
   AcademicYearGetAllEndpoint
 } from '../../../../../endpoints/academic-year-endpoints/academic-year-get-all-endpoint.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {
+  StudentUpdateOrInsertRequest
+} from '../../../../../endpoints/student-endpoints/student-update-or-insert-endpoint.service';
+import {
+  SemesterUpdateOrInsertEndpoint,
+  SemesterUpdateOrInsertRequest
+} from '../../../../../endpoints/semester-endpoints/semester-update-or-insert-endpoint.service';
 
 @Component({
   selector: 'app-student-semesters-new',
@@ -22,14 +30,29 @@ export class StudentSemestersNewComponent implements OnInit {
   student:any;
   academyYears:any;
 
+  semesterForm: FormGroup;
+
   constructor(private route:ActivatedRoute,
               private studentGetByIdService:StudentGetByIdEndpointService,
               private snackbar: MySnackbarHelperService,
               private router: Router,
               private dialog: MatDialog,
-              private AcademicYearGetAllService:AcademicYearGetAllEndpoint
+              private AcademicYearGetAllService:AcademicYearGetAllEndpoint,
+              private fb: FormBuilder,
+              private semesterUpdateOrInsertService:SemesterUpdateOrInsertEndpoint
               ) {
     this.studentId = route.snapshot.params['id'];
+
+    this.semesterForm = this.fb.group({
+        studentId: [this.studentId, [Validators.required]],
+        recordedById: [1, [Validators.required]],
+        academicYearId: [1, [Validators.required]],
+        date:[new Date(), [Validators.required]],
+        yearOfStudy: [1, [Validators.required]],
+        price: [null, [Validators.required, Validators.min(50), Validators.max(2000)   ]],
+        renewal: [null, [Validators.required]],
+    });
+
   }
 
 
@@ -65,6 +88,28 @@ export class StudentSemestersNewComponent implements OnInit {
         console.error('Error fetching academic years:', err);
       }
     });
+
+  }
+
+  saveSemester() {
+
+    if (this.semesterForm.invalid) return;
+
+    const semesterData: SemesterUpdateOrInsertRequest = {
+      ...this.semesterForm.value,
+    };
+
+    this.semesterUpdateOrInsertService.handleAsync(semesterData).subscribe({
+      next: () => {
+        this.snackbar.showMessage('Semester added succesfully.', 5000);
+        this.router.navigate(['/admin/students/semesters/', this.studentId]);
+      },
+      error: (error) => {
+        this.snackbar.showMessage('Error adding semester. Please try again.', 5000);
+        console.error('Error adding semester', error);
+      },
+    });
+
 
   }
 }
