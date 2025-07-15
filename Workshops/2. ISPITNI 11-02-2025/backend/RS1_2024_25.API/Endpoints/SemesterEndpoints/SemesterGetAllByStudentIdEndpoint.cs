95b1a1b75a23f19fdc58d37c9ecd7e301f465b84
Vedad Keskin <vedad.keskin98@gmail.com>
2025-07-15 14:58:50 +0200

    Initial commit: backend and frontend project setup
    Add initial implementation for RS1_2024_25 project, including backend ASP.NET Core API with authentication, data models, endpoints, migrations, and helper classes, as well as Angular frontend with routing, modules, services, and UI components for admin, auth, client, and public areas.

---
+++ Workshops/2. ISPITNI 11-02-2025/backend/RS1_2024_25.API/Endpoints/SemesterEndpoints/SemesterGetAllByStudentIdEndpoint.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RS1_2024_25.API.Data;
+using RS1_2024_25.API.Data.Models.SharedTables;
+using RS1_2024_25.API.Data.Models.TenantSpecificTables.Modul1_Auth;
+using RS1_2024_25.API.Helper.Api;
+using static RS1_2024_25.API.Endpoints.SemesterEndpoints.SemesterGetAllByStudentIdEndpoint;
+
+namespace RS1_2024_25.API.Endpoints.SemesterEndpoints;
+
+
+//bez paging i bez filtera
+[Route("semesters")]
+public class SemesterGetAllByStudentIdEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<int>
+    .WithResult<SemesterGetAllByStudentIdResponse[]>
+{
+    [HttpGet("{id}")]
+    public override async Task<SemesterGetAllByStudentIdResponse[]> HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var result = await db.Semesters
+            .Where(x=> x.StudentId == id)
+            .Include(x=> x.AcademicYear)
+            .Include(x=> x.RecordedBy)
+                        .Select(c => new SemesterGetAllByStudentIdResponse
+                        {
+                            ID = c.ID,
+                            AcademicYearDescription = $"{c.AcademicYear!.StartDate.Year}-{c.AcademicYear.EndDate.Year % 100}",
+                            YearOfStudy = c.YearOfStudy,
+                            Renewal = c.Renewal,
+                            DateOfEnrollment = c.DateOfEnrollment,
+                            RecordedByName = c.RecordedBy!.Email,
+                        })
+                        .ToArrayAsync(cancellationToken);
+
+        return result;
+    }
+
+    public class SemesterGetAllByStudentIdResponse
+    {
+        public required int ID { get; set; }
+        public string? AcademicYearDescription { get; set; }
+        public int YearOfStudy { get; set; }
+        public bool Renewal { get; set; }
+        public DateTime DateOfEnrollment { get; set; }
+        public string? RecordedByName { get; set; }
+
+
+    }
+}
